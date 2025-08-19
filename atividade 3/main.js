f58bd4efdba1427d7ad6752a89283da5de7f65d5
becuda16 <rebeca.chirmicci.silva@escola.pr.gov.br>
2025-08-19 09:21:15 -0300

    Add files via upload
    19

--- atividade 3/main.js
+++ atividade 3/main.js
@@ -1,16 +1,15 @@
 const frm = document.querySelector("form");
-
 const resp1 = document.querySelector("#out Resp1");
 const resp2 = document.querySelector("#outResp2");
 const resp3 = document.querySelector("#outResp3");
 
 frm.addEventListener("submit", (e) => {
 
-const veiculo frm.inVeiculo.value;
-
+const veiculo = frm.inVeiculo.value;
 const preco = Number(frm.inPreco.value);
+
 const entrada = preco * 0.50;
-const parcela (preco * 0.50) / 12;
+const parcela = (preco * 0.50) / 12;
 
 resp1.innerText = `Promoção: ${veiculo}`;
 resp2.innerText = `Entrada de R$: ${entrada.toFixed(2)}`;
--- atividade 4/main.js
+++ atividade 4/main.js
@@ -1,29 +1,19 @@
 
-
- Arquivo main.js
-// cria referência ao form e aos elementos de resposta (pelo seu id)
 const frm = document.querySelector("form");
-const resp1= document.querySelector("#outResp1");
-const resp2 document.querySelector("#outResp2");
-
-const resp3= document.querySelector("#outResp3");
-// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+const resp3 = document.querySelector("#outResp3");
 
 frm.addEventListener("submit", (e) => {
-const veiculo frm.inVeiculo.value;
-// obtém o conteúdo dos campos
+const veiculo = frm.inVeiculo.value;
+const preco = Number(frm.inPreco.value);
 
-const preco = Number(frm.inPreco.
-const entrada preco 0.50;
-// calcula valor da entrada
-const parcela (preco 0.50) / 12;
-// e das parcelas
-resp1.innerText = Promoção: $(veiculo);
-// exibe as respostas
-resp2.innerText = Entrada de R$: $(entrada.toFixed(2)}^;
+const entrada = preco * 0.50;
+const parcela = (preco * 0.50) / 12;
 
-resp3.innerText +12x de R$ ${parcela.toFixed(2)];
+resp1.innerText = `Promoção: ${veiculo}`;
+resp2.innerText = `Entrada de R$: ${entrada.toFixed(2)}`;
+resp3.innerText = `+12x de R$ ${parcela.toFixed(2)}`;
 
 e.preventDefault();
-// evita envio do form
 })
